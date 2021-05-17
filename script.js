2397890189f5ca237e651ea975dcd371b3820e1e
Ekremina Arik <ekreminaarik@gmail.com>
2021-05-17 23:24:33 +0100

    Adding score to the current

--- script.js
+++ script.js
@@ -3,11 +3,40 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //getting element by ID
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-//setting them to 0
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Setting scores to 0
 score0El.textContent = 0;
 score1El.textContent = 0;
 
-//hiding the dice
+//Hiding the dice
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //Display the dice
+  diceEl.classList.remove('hidden');
+  //Display the images
+  diceEl.src = `dice-${dice}.png`;
+
+  //Check for rolled 1
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later!!!
+  } else {
+    //switch to the next player
+  }
+});
